f80c1b9efabf6f596449ea292705b4e4c98ce8dc
anqtran <anqtran@gatech.edu>
2019-08-31 16:11:35 -0400

    add api routes for auth and users

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
 
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
