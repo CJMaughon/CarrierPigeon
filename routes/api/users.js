095a9b5a2fc539b0c468959939f1900bca3afe79
anqtran <anqtran@gatech.edu>
2019-09-01 01:15:48 -0400

    add endpoint to register new user to database

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const {check, validationResult } = require("express-validator");
-
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
 // @route 	POST api/users
 // @desc	Test route
 // @access 	Public
@@ -17,13 +18,41 @@ router.post(
 		'Please enter a password with 6 or more characters'
 		).isLength({min: 6})
 	],
-	(req, res) => {
+	async (req, res) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
+		const { name, email, password} = req.body;
 
-		res.send('User route')
+		try {
+			// See if user exists
+			let user = await User.findOne({ email });
+
+			if (user) {
+				return res.status(400).json({ errors : [{msg: 'User already exists' }] });
+			}
+
+		    user = new User({
+		    	name,
+		    	email,
+		    	password
+		    })
+
+			// Encrypt password
+			const salt = await bcrypt.genSalt(10);
+			user.password = await bcrypt.hash(password, salt);
+
+			await user.save();
+			// Return jsonwebtoken
+
+			res.send('User registered')
+		} catch(err) {
+			console.log(err.message);
+			res.status(500).send('Server error');
+		}
+
+		
 	}
 	);
 module.exports = router;
