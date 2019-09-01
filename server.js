7d2690ba41242f3dec449517b84070713b7b3ff3
anqtran <anqtran@gatech.edu>
2019-09-01 00:47:58 -0400

    fix user model and add post route for user

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
 
+// Init Middleware
+app.use(express.json({extended: false}));
+
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 
