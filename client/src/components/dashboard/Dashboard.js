c93d125f6b2a3fd037ef63dc77a8e4c16d4819ae
anqtran <anqtran@gatech.edu>
2019-09-06 14:37:14 -0400

    add logout function

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
-const Landing = () => {
+import { logout } from '../../actions/auth';
+import PropTypes from 'prop-types';
+const Dashboard = ({ logout }) => {
   return (
     <section className='landing'>
       <div className='dark-overlay'>
         <div className='landing-inner'>
           <h1 className='x-large'>Instructor Dashboard</h1>
+          <button onClick={logout}>LOG OUT</button>
         </div>
       </div>
     </section>
   );
 };
 
-export default connect(null)(Landing);
+Dashboard.propTypes = {
+  logout: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { logout }
+)(Dashboard);
