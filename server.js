2a2154db960f129645a306a89549049da1243c9a
Clayton <cjm@localhost.localdomain>
2019-09-25 19:56:48 -0400

    Can view files from drive just running the server. Testing now

--- server.js
+++ server.js
@@ -3,17 +3,50 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-// Connect Database
-connectDB();
+const { google } = require('googleapis');
 
-app.get('/', (req, res) => res.send('API running'));
+const credentials = require('./credentials.json');
 
-// Init Middleware
-app.use(express.json({ extended: false }));
+const scopes = [
+    'https://www.googleapis.com/auth/drive'
+  ];
+  
+const auth = new google.auth.JWT(
+    credentials.client_email, null,
+    credentials.private_key, scopes
+);
 
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
+const drive = google.drive({version: 'v3', auth});
+drive.files.list({
+    pageSize: 10,
+    fields: 'nextPageToken, files(id, name)',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const files = res.data.files;
+    if (files.length) {
+      console.log('Files:');
+      files.map((file) => {
+        console.log(`${file.name} (${file.id})`);
+      });
+    } else {
+      console.log('No files found.');
+    }
+  });
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
+
+
+// // Connect Database
+// connectDB();
+
+// app.get('/', (req, res) => res.send('API running'));
+
+// // Init Middleware
+// app.use(express.json({ extended: false }));
+
+// app.use('/api/users', require('./routes/api/users'));
+// app.use('/api/auth', require('./routes/api/auth'));
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
